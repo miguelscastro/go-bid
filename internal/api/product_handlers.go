766271b24b92d58677dd5795272f2cb083efe019
miguelscastro <miguelscastro18@gmail.com>
2026-03-16 16:23:44 -0300

    chore: setup product routes and migrations

---
+++ internal/api/product_handlers.go
@@ -0,0 +1,7 @@
+package api
+
+import "net/http"
+
+func (api *Api) HandleCreateProduct(w http.ResponseWriter, r *http.Request) {
+	panic("TODO - NOT IMPLEMENTED")
+}
--- internal/api/routes.go
+++ internal/api/routes.go
@@ -1,11 +1,8 @@
 package api
 
 import (
-	"os"
-
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/gorilla/csrf"
 )
 
 func (api *Api) BindRoutes() {
@@ -15,20 +12,23 @@ func (api *Api) BindRoutes() {
 		middleware.Logger,
 		api.Sessions.LoadAndSave)
 
-	csrfMiddleware := csrf.Protect(
-		[]byte(os.Getenv("GOBID_CSRF_KEY")),
-		csrf.Path("/api/v1"),
-		csrf.Secure(false)) // dev only -> http is not secure
+	// csrfMiddleware := csrf.Protect(
+	// 	[]byte(os.Getenv("GOBID_CSRF_KEY")),
+	// 	csrf.Path("/api/v1"),
+	// 	csrf.Secure(false))
 
-	api.Router.Use(csrfMiddleware) // checks POST, PATCH, PUT, DELETE only
+	// api.Router.Use(csrfMiddleware)
 
 	api.Router.Route("/api", func(r chi.Router) {
 		r.Route("/v1", func(r chi.Router) {
-			r.Get("/csrftoken", api.handleGetCSRFToken)
+			// r.Get("/csrftoken", api.handleGetCSRFToken)
 			r.Route("/users", func(r chi.Router) {
 				r.Post("/signup", api.handleSignupUser)
 				r.Post("/login", api.handleLoginUser)
 				r.With(api.AuthMiddleware).Post("/logout", api.handleLogoutUser)
+			})
+			r.Route("/products", func(r chi.Router) {
+				r.Post("/", api.HandleCreateProduct)
 			})
 		})
 	})
